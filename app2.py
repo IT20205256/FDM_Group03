ca6e1b9a91cc76a3ae289fa8d11843c84f2becb0
Kishan R <89646459+IT20205256@users.noreply.github.com>
2022-11-02 12:09:48 +0530

    Updated

--- app2.py
+++ app2.py
@@ -20,7 +20,7 @@ def predict():
     
     return render_template('Index.html', prediction_text= 'Is this claim is a Fraud:{}'.format(output).replace('0','No').replace('1','Yes'))
 
-@app.route('/View', methods=['POST'])
+@app.route('/View', methods=['POST', 'GET'])
 def View():
     return render_template('view.html')
 
